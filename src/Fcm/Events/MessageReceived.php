35a468e8d6bb0f8011c2eb09daf6517ef80550bd
Mpande Andrew <andrewmvp007@gmail.com>
2018-04-30 19:33:44 +0300

    added message channel

--- src/Fcm/Events/MessageReceived.php
+++ src/Fcm/Events/MessageReceived.php
@@ -11,12 +11,19 @@ class MessageReceived
     public $mData = [];
 
     /**
+     * @var string
+     */
+    public $channel;
+
+    /**
      * Create a new event instance.
      *
      * @param $mData
+     * @param $channel
      */
-    public function __construct($mData)
+    public function __construct($mData, $channel)
     {
         $this->mData = $mData;
+        $this->channel = $channel;
     }
 }
